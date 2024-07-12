2f443ad3601e22fa02c47d1588f95a53088eb97b
Sung-Numa03 <sungnumafroko@gmail.com>
2024-07-13 00:20:03 +0100

    Added a middleware to the friends route

--- routes/friends.router.js
+++ routes/friends.router.js
@@ -4,6 +4,10 @@ const friendsController = require('../controllers/friends.controller');
 
 const friendsRouter = express.Router();
 
+friendsRouter.use((req, res, next) => {
+    console.log('Ip Address: ', req.ip);
+    next();
+})
 friendsRouter.post('/', friendsController.postFriends);
 friendsRouter.get('/', friendsController.getFriends);
 friendsRouter.get('/:friendId', friendsController.getFriend);
